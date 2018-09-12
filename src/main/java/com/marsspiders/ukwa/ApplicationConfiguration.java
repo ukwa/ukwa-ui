b3c4c9883092e6acc86ddeebe755701265975595
Mindaugas Vidmantas <minvidm@gmail.com>
2018-09-12 16:54:46 +0100

    Application Configuration add Beans

--- src/main/java/com/marsspiders/ukwa/ApplicationConfiguration.java
+++ src/main/java/com/marsspiders/ukwa/ApplicationConfiguration.java
@@ -1,5 +1,9 @@
 package com.marsspiders.ukwa;
 
+import com.marsspiders.ukwa.ip.WaybackIpResolver;
+import com.marsspiders.ukwa.solr.SolrCommunicator;
+import com.marsspiders.ukwa.solr.SolrSearchService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -7,6 +11,10 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
 
 @SpringBootApplication
 public class ApplicationConfiguration extends SpringBootServletInitializer {
@@ -27,5 +35,52 @@ public class ApplicationConfiguration extends SpringBootServletInitializer {
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
     }
+
+
+    @Bean
+    SolrSearchService solrSearchService() {
+        return new SolrSearchService();
+    }
+    @Bean
+    SolrCommunicator solrCommunicator() {
+        return new SolrCommunicator();
+    }
+    @Bean
+    WaybackIpResolver waybackIpResolver() {
+        return new WaybackIpResolver();
+    }
+    @Bean
+    WaybackIpConfiguration waybackIpConfiguration() {
+        return new WaybackIpConfiguration();
+    }
+    /*
+    @Bean
+    JavaMailSender javaMailSender() {
+        return new JavaMailSender();
+    }
+    */
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        /*
+        mailSender.setHost("smtp.gmail.com");//spring.mail.host
+        mailSender.setPort(587);
+        mailSender.setUsername("my.gmail@gmail.com");
+        mailSender.setPassword("password");
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+        */
+
+        return mailSender;
+    }
+
+
+
+
 
 }
