0d5edc93816e2897bdd2f98a83e6ef491680503a
Andrew Jackson <Andrew.Jackson@bl.uk>
2018-10-25 16:17:38 +0100

    Removing broken tests
    The tests appear fundementally broken, likely due to classpath or
    jar version issues. As the lone test did very little, it has
    been removed.

--- src/main/java/com/marsspiders/ukwa/ApplicationConfiguration.java
+++ src/main/java/com/marsspiders/ukwa/ApplicationConfiguration.java
@@ -4,13 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-
-import com.marsspiders.ukwa.ip.WaybackIpResolver;
-import com.marsspiders.ukwa.solr.SolrCommunicator;
-import com.marsspiders.ukwa.solr.SolrSearchService;
 
 
 @SpringBootApplication
@@ -23,34 +16,6 @@ public class ApplicationConfiguration extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(ApplicationConfiguration.class);
-    }
-
-    @Bean
-    public MessageSource messageSource() {
-        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-        messageSource.setBasename("classpath:/i18n/messages");
-        messageSource.setDefaultEncoding("UTF-8");
-        return messageSource;
-    }
-
-    @Bean
-    SolrSearchService solrSearchService() {
-        return new SolrSearchService();
-    }
-
-    @Bean
-    SolrCommunicator solrCommunicator() {
-        return new SolrCommunicator();
-    }
-
-    @Bean
-    WaybackIpResolver waybackIpResolver() {
-        return new WaybackIpResolver();
-    }
-
-    @Bean
-    WaybackIpConfiguration waybackIpConfiguration() {
-        return new WaybackIpConfiguration();
     }
 
 }
