cf0239d930624b82e2cc21eb7e9510fbbb163724
min2ha <minvidm@gmail.com>
2020-04-06 19:27:57 +0300

    Resolve WCAG 2.0 issues from second AbilityNet report #246 (spring session)

--- src/main/java/com/marsspiders/ukwa/ApplicationConfiguration.java
+++ src/main/java/com/marsspiders/ukwa/ApplicationConfiguration.java
@@ -7,6 +7,8 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 
 
 @SpringBootApplication
@@ -27,5 +29,14 @@ public class ApplicationConfiguration extends SpringBootServletInitializer {
         messageSource.setBasename("classpath:/i18n/messages");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
+    }
+
+    @Bean
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setCookieName("highcontrast");
+        serializer.setCookiePath("/");
+        serializer.setDomainNamePattern("^.+?\\.(\\w+\\.[a-z]+)$");
+        return serializer;
     }
 }
